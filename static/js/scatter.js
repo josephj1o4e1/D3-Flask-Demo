1f1191571c7f8d0e0204cdfe9b35b5f1224a4463
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-06-27 17:50:01 -0400

    code restructure(leave only handleYearFilter inside d3.csv) and add clipping to the dots element.

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -1,3 +1,15 @@
+// 1. create x-y axis
+// 2. create yearSlider input element with initial values. 
+// 3. zoom and pan
+// 4. read data using d3.csv, and create a callback function "handleYearFilter" for d3.csv: 
+    // - filter by year
+    // - groupby on category with aggregation avg. backers & pledges
+// 4. add input-type eventlistener on yearSlider. when triggered, execute handleYearFilter again. 
+// 5. first execution of handleYearFilter. 
+
+
+
+
 // Define the dimensions and margins for the plot
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 40, left: 80},
@@ -13,72 +25,119 @@ const svg = d3.select("#chart")
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Add X axis
+const x = d3.scaleLinear()
+    .domain([0, 200])
+    .range([ 0, width ]);
+xAxis = svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+// Add X axis label:
+svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height + margin.top + 25)
+    .text("Backers Count");
+
+// Add Y axis
+const y = d3.scaleLinear()
+    .domain([0, 100000])
+    .range([ height, 0]);
+yAxis = svg.append("g")
+    .call(d3.axisLeft(y));
+// Y axis label:
+svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left+20)
+    .attr("x", -margin.top)
+    .text("Pledges (USD)")
+
+// Add a clipPath: everything out of this area won't be drawn.
+let clip = svg.append("defs")
+  .append("svg:clipPath")
+  .attr("id", "clip")
+  .append("svg:rect")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("x", 0)
+  .attr("y", 0);
+
+// Create the dots element under svg, where datapoint dots would take place (and also possibly a brush feature)
+dots = svg.append("g").attr("clip-path", "url(#clip)");
+
+// Get yearSlider input element in the DOM.
+const yearSlider = document.getElementById("yearSlider");
+const yearIndicator = document.getElementById("yearIndicator");
+yearSlider.min = 2009; // Minimum year value
+yearSlider.max = 2020; // Maximum year value
+yearSlider.step = 1; // Increment step
+// yearSlider.value = "all"; // "all"; // Initial value set to "all"
+yearSlider.list = "yearselector";
+yearSlider.setAttribute("list", "yearselector");
+yearIndicator.innerHTML = yearSlider.value;
+
+// Zoom and Pan
+function handleZoom(e) {
+    const verticalTranslation = `translate(${e.transform.x}, ${e.transform.y + height*e.transform.k}) scale(${e.transform.k})`;
+    // apply transform to the chart / dots
+    dots.attr('transform', e.transform); // e.transform has three properties x, y and k. (k is the scale factor), pass e.transform directly into .attr
+    // Apply the same transform to the axes
+    xAxis.attr('transform', verticalTranslation); // xAxis.attr('transform', e.transform);
+    yAxis.attr('transform', e.transform);
+}
+
+let zoom = d3.zoom()
+    .scaleExtent([0.5, 4]) // Set the minimum and maximum zoom scale
+    // .translateExtent([[0, 0], [width, height]]) // to specify bounds [[x0, y0], [x1, y1]] that the user can't pan outside of
+    .translateExtent([[-20, 0], [600, 400]])
+    .on('zoom', handleZoom);
+
+function initZoom() {
+    d3.select('svg')
+        .call(zoom);
+}
+
+initZoom();
+
+// function updateChart(newX, newY) {
+//     // let t = SVG.transition().duration(750);
+//     anim = d3.select('svg').transition().duration(750);
+    
+//     // update axes with these new boundaries
+//     xAxis.transition(anim).call(d3.axisBottom(newX));
+//     yAxis.transition(anim).call(d3.axisLeft(newY));
+    
+//     // update circle position
+//     scatter
+//         .selectAll("circle")
+//         .transition(anim)
+//         .attr("cx", function(d) {
+//             return newX(d.Sepal_Length);
+//         })
+//         .attr("cy", function(d) {
+//             return newY(d.Petal_Length);
+//         });
+// }
+
+// function reset_zoom() {
+//     newX = x.domain(x0);
+//     newY = y.domain(y0);
+    
+//     updateChart(newX, newY);
+// }
+
+// let resetbutton = document.getElementById("resetZoom"); 
+// resetbutton.addEventListener("click", reset_zoom);
+
 
 //Read the data
 d3.csv("static/data/ProjectDataUSA-week3.csv").then( function(data) {
-
-    // 1. create x-y axis
-    // 2. create yearSlider input element with initial values. 
-    // 3. create the handleYearFilter function: 
-        // - year filter 
-        // - groupby on category with aggregation avg. backers & pledges
-    // 4. add input-type eventlistener on yearSlider. when triggered, execute handleYearFilter again. 
-    // 5. first execution of handleYearFilter. 
-
-
-    // Add X axis
-    const x = d3.scaleLinear()
-        .domain([0, 200])
-        .range([ 0, width ]);
-    xAxis = svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-    // Add X axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height + margin.top + 25)
-        .text("Backers Count");
-
-    // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, 100000])
-        .range([ height, 0]);
-    yAxis = svg.append("g")
-        .call(d3.axisLeft(y));
-    // Y axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left+20)
-        .attr("x", -margin.top)
-        .text("Pledges (USD)")
-
-
-    // create yearSlider input element in the DOM with initial values.
-    // const yearSlider = document.createElement("input");
-    // const yearIndicator = document.createElement("div");
-    const yearSlider = document.getElementById("yearSlider");
-    const yearIndicator = document.getElementById("yearIndicator");
-    yearSlider.min = 2009; // Minimum year value
-    yearSlider.max = 2020; // Maximum year value
-    yearSlider.step = 1; // Increment step
-    // yearSlider.value = "all"; // "all"; // Initial value set to "all"
-    yearSlider.list = "yearselector";
-    yearSlider.setAttribute("list", "yearselector");
-    // d3.select("#filters").node().appendChild(yearSlider);
-    // d3.select("#filters").node().appendChild(yearIndicator);
-
-    dots = svg.append("g");
-    // let groupedData;
-    // create the handleYearFilter function
+    // create function to handle the yearSlider filter updates. 
     function handleYearFilter() {
-
-        const selectedValue = yearSlider.value;
+        let selectedValue = yearSlider.value;
         yearIndicator.innerHTML = selectedValue;
         let filteredData;
-        
-        console.log(selectedValue);
         // Check if "all years" is selected
         if (selectedValue === "all") {
             // Use the entire data set
@@ -89,8 +148,7 @@ d3.csv("static/data/ProjectDataUSA-week3.csv").then( function(data) {
             // Filter the data based on the selected year range
             filteredData = data.filter((d) => d.LAUNCHED_YEAR >= startYear);
         }
-        console.log(filteredData);
-
+        
         // Aggregate data by category and calculate averages of Backers & Pledges
         const aggregatedData = d3.rollup(filteredData, 
             v => ({
@@ -110,8 +168,6 @@ d3.csv("static/data/ProjectDataUSA-week3.csv").then( function(data) {
             .domain([0, 1]) // Assuming success rate is between 0 and 1
             .range(["yellow", "red"]); // Adjust the colors as needed
         
-        // // Clear the previous dots
-        // svg.selectAll(".dot").remove();
 
         // Append circles for datapoints 'dots' 
         dots.selectAll("circle")
@@ -138,31 +194,7 @@ d3.csv("static/data/ProjectDataUSA-week3.csv").then( function(data) {
         // // Label collision detection and adjustment
 
     }
-
-    // add input-type eventlistener on yearSlider. when triggered, it'll execute handleYearFilter again. 
+    // add eventlistener on yearSlider.  
     yearSlider.addEventListener("input", handleYearFilter);
-
-    // Zoom and Pan
-    function handleZoom(e) {
-        const verticalTranslation = `translate(${e.transform.x}, ${e.transform.y + height}) scale(${e.transform.k})`;
-        // apply transform to the chart / dots
-        dots.attr('transform', e.transform); // e.transform has three properties x, y and k. (k is the scale factor), pass e.transform directly into .attr
-        // Apply the same transform to the axes
-        xAxis.attr('transform', verticalTranslation);
-        yAxis.attr('transform', e.transform);
-    }
-    
-    let zoom = d3.zoom()
-        .scaleExtent([0.5, 4]) // Set the minimum and maximum zoom scale
-        .translateExtent([[0, 0], [width, height]]) // to specify bounds [[x0, y0], [x1, y1]] that the user can't pan outside of
-        .on('zoom', handleZoom);
-
-    function initZoom() {
-        d3.select('svg')
-            .call(zoom);
-    }
-    
-    initZoom();
-    handleYearFilter(); // updateData: first execution of handleYearFilter. 
-
+    handleYearFilter(); // updateData: first execution of handleYearFilter.
 })
